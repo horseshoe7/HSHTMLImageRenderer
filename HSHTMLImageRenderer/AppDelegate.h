3097157765558a0dc92df7ec00278919cbe645a5
horseshoe7 <ocon.sc@gmail.com>
2016-04-15 19:08:06 +0200

    Initial Commit

---
+++ HSHTMLImageRenderer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HSHTMLImageRenderer
+//
+//  Created by Stephen O'Connor on 15/04/16.
+//  Copyright © 2016 Software Barn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HSHTMLImageRenderer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HSHTMLImageRenderer
+//
+//  Created by Stephen O'Connor on 15/04/16.
+//  Copyright © 2016 Software Barn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
